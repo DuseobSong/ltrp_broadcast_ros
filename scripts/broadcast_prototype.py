93492e768d929c73755eb17383b3703faa7b42b1
DuseobSong <duseobg.song@outlook.kr>
2025-04-10 15:12:22 +0900

    UPDATE [2025.04.10]

--- scripts/broadcast_prototype.py
+++ scripts/broadcast_prototype.py
@@ -17,73 +17,75 @@ rp = RosPack()
 
 class Broadcaster:
     def __init__(self, root_dir, debug, self_pkg_name):
-        self.DEBUG = debug
-        self.err_code = 2
-        self.node_name = "BROADCAST"
-        self.node_code = None
-        self.operation = True
-        self.rate1 = rospy.Rate(20)
-        self.rate2 = rospy.Rate(20)
-        self.system_ready = 9
+        self.DEBUG                      = debug
+        self.err_code                   = 2
+        self.node_name                  = "BROADCAST"
+        self.node_code                  = None
+        self.operation                  = True
+        self.rate1                      = rospy.Rate(20)
+        self.rate2                      = rospy.Rate(20)
+        self.system_ready               = 9
         
-        self.error_code_list_dir = root_dir + '/config/error_code_list.json'
-        self.parameter_dir = root_dir + '/config/parameters.json'
-        self.topic_list_dir = root_dir + '/config/topic_list.json'
+        self.error_code_list_dir        = root_dir + '/config/error_code_list.json'
+        self.parameter_dir              = root_dir + '/config/parameters.json'
+        self.topic_list_dir             = root_dir + '/config/topic_list.json'
         
-        self.error_code_list = None
-        self.parameters = None
-        self.topic_list = None
+        self.error_code_list            = None
+        self.parameters                 = None
+        self.topic_list                 = None
         
         # Broadcast
-        self.sound_file_dir = rp.get_path(self_pkg_name) + '/config/sound_files/'
-        self.SOUND_FILE_CODES = None
-        self.SOUND_FILE_LIST = None
+        self.sound_file_dir             = rp.get_path(self_pkg_name) + '/config/sound_files/'
+        self.SOUND_FILE_CODES           = None
+        self.SOUND_FILE_LIST            = None
         
-        self.broadcast_request = None
-        self.on_air_flag = False
-        self.repeat_flag = False
+        self.broadcast_request          = None
+        self.on_air_flag                = False
+        self.repeat_flag                = False
         
-        self.NODE_CODES = None
-        self.COMMON_DEFAULT_VALUES = None
-        self.DEFAULT_VALUES = None
-        self.SYSTEM_SHUTDOWN_CODE = None
-        self.REMOTE_CONTROL_COMMAND = None
+        self.NODE_CODES                 = None
+        self.COMMON_DEFAULT_VALUES      = None
+        self.DEFAULT_VALUES             = None
+        self.SYSTEM_SHUTDOWN_CODE       = None
+        self.REMOTE_CONTROL_COMMAND     = None
         
-        self.NODE_STATE_ERROR = 9
-        self.NODE_STATE_OFFLINE = None
-        self.NODE_STATE_ONLINE = None
+        self.NODE_STATE_ERROR           = 9
+        self.NODE_STATE_OFFLINE         = None
+        self.NODE_STATE_ONLINE          = None
         
-        self.broadcast_topic = None
-        self.error_topic = '/ltrp/error'
-        self.node_respawn_result_topic = '/ltrp/respawn_result'
-        self.remote_control_topic   = None
-        self.system_shutdown_topic = None
-        self.system_status_topic = None
-        self.vital_topic = None
+        self.broadcast_topic            = None
+        self.error_topic                = '/ltrp/error'
+        self.node_respawn_result_topic  = '/ltrp/respawn_result'
+        self.remote_control_topic       = None
+        self.system_shutdown_topic      = None
+        self.system_status_topic        = None
+        self.vital_topic                = None
         
-        self.boradcast_sub = None
-        self.remote_control_sub = None
-        self.system_shutdown_sub = None
-        self.system_status_sub = None
+        self.boradcast_sub              = None
+        self.remote_control_sub         = None
+        self.system_shutdown_sub        = None
+        self.system_status_sub          = None
         
-        self.broadcast_pub = None
-        self.error_pub = rospy.Publisher(self.error_topic, Error, queue_size=10)
-        self.node_respawn_result_pub = rospy.Publisher(self.node_respawn_result_topic, NodeRespawnResult, queue_size=10)
+        self.broadcast_pub              = None
+        self.error_pub                  = rospy.Publisher(self.error_topic, Error, 
+                                                          queue_size=10)
+        self.node_respawn_result_pub    = rospy.Publisher(self.node_respawn_result_topic, 
+                                                          NodeRespawnResult, queue_size=10)
         self.vital_pub = None
         
         
-        self.info_msg_broadcast_flag        = 0 # 0: brfore rotation / before broadcasting
-                                                # 1: on rotation     / before broadcasting
-                                                # 2: rotation_done   / before broadcasting
-                                                # 3: rotation done   / on broadcasting 
-                                                # 4: rotation_done   / broadcasting done
-                                                # 5: on rotation     / broadcasting done
-                                                # 6: rotation done   / broadcasting done
+        self.info_msg_broadcast_flag    = 0 # 0: brfore rotation / before broadcasting
+                                            # 1: on rotation     / before broadcasting
+                                            # 2: rotation_done   / before broadcasting
+                                            # 3: rotation done   / on broadcasting 
+                                            # 4: rotation_done   / broadcasting done
+                                            # 5: on rotation     / broadcasting done
+                                            # 6: rotation done   / broadcasting done
                                                 
-        self.lock = threading.Lock()
+        self.lock                       = threading.Lock()
         
-        self.broadcast_thread = None
-        self.communication_thread = threading.Thread(target=self.communication_task, args=())
+        self.broadcast_thread           = None
+        self.communication_thread       = threading.Thread(target=self.communication_task, args=())
         
     def raise_error(self, error_code, exception=None):
         self.err_code = error_code
@@ -101,10 +103,11 @@ class Broadcaster:
         self.operation = False
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print(exc_type, fname, exc_tb.tb_lineno)
+        line_no = exc_tb.lineno
+        print(exc_type, fname, line_no)
         
         self.publish_response()
-        self.publish_error_msg()
+        self.publish_error_msg(exc_type, fname, line_no)
         
     def load_error_code_list(self):
         try:
@@ -151,29 +154,34 @@ class Broadcaster:
             return False
         
     def set_default_values(self):
-        self.err_code = self.COMMON_DEFAULT_VALUES["DEFAULT_ERR_CODE"]
-        self.NODE_STATE_ERROR = self.COMMON_DEFAULT_VALUES["NODE_STATE_ERROR"]
-        self.NODE_STATE_OFFLINE = self.COMMON_DEFAULT_VALUES["NODE_STATE_OFFLINE"]
-        self.NODE_STATE_ONLINE = self.COMMON_DEFAULT_VALUES["NODE_STATE_ONLINE"]
-        self.SYSTEM_SHUTDOWN_CODE = self.COMMON_DEFAULT_VALUES["SYSTEM_SHUTDOWN_CODE"]
+        self.rate1                  = rospy.Rate(self.DEFAULT_VALUES["BROADCAST_THREAD_HZ"])
+        self.rate1                  = rospy.Rate(self.DEFAULT_VALUES["COMMUNICATION_THREAD_HZ"])
+        
+        self.err_code               = self.COMMON_DEFAULT_VALUES["DEFAULT_ERR_CODE"]
+        self.NODE_STATE_ERROR       = self.COMMON_DEFAULT_VALUES["NODE_STATE_ERROR"]
+        self.NODE_STATE_OFFLINE     = self.COMMON_DEFAULT_VALUES["NODE_STATE_OFFLINE"]
+        self.NODE_STATE_ONLINE      = self.COMMON_DEFAULT_VALUES["NODE_STATE_ONLINE"]
+        self.SYSTEM_SHUTDOWN_CODE   = self.COMMON_DEFAULT_VALUES["SYSTEM_SHUTDOWN_CODE"]
         
     def set_publisher(self):
-        self.broadcast_pub = rospy.Publisher(self.broadcast_topic, Broadcast, queue_size=10)
-        self.vital_pub = rospy.Publisher(self.vital_topic, Vital, queue_size=10)
+        self.broadcast_pub          = rospy.Publisher(self.broadcast_topic, Broadcast,
+                                                      queue_size=10)
+        self.vital_pub              = rospy.Publisher(self.vital_topic, Vital, queue_size=10)
         
     def set_subscriber(self):
-        self.broadcast_sub = rospy.Subscriber(self.broadcast_topic, Broadcast, self.broadcast_callback)
-        self.remote_control_sub = rospy.Subscriber(self.remote_control_topic, UInt8, self.remote_control_callback)    
-        self.system_shutdown_sub = rospy.Subscriber(self.system_shutdown_topic, UInt8, self.system_shutdown_callback)
-        self.system_status_sub = rospy.Subscriber(self.system_status_topic, SystemStatus, self.system_status_callback)
+        self.broadcast_sub          = rospy.Subscriber(self.broadcast_topic, Broadcast, 
+                                                       self.broadcast_callback)
+        self.remote_control_sub     = rospy.Subscriber(self.remote_control_topic, UInt8, 
+                                                       self.remote_control_callback)    
+        self.system_shutdown_sub    = rospy.Subscriber(self.system_shutdown_topic, UInt8, 
+                                                       self.system_shutdown_callback)
         
     def set_topic(self):
-        prefix = self.topic_list["prefix"]
-        self.broadcast_topic = prefix + self.topic_list["broadcast_topic"]["NAME"]
-        self.remote_control_topic = prefix + self.topic_list["remote_control_topic"]["NAME"]
-        self.system_shutdown_topic = prefix + self.topic_list["system_shutdown_topic"]["NAME"]
-        self.system_status_topic = prefix + self.topic_list["system_status_topic"]["NAME"]
-        self.vital_topic = prefix + self.topic_list["vital_topic"]["NAME"]
+        prefix                      = self.topic_list["prefix"]
+        self.broadcast_topic        = prefix + self.topic_list["broadcast_topic"]["NAME"]
+        self.remote_control_topic   = prefix + self.topic_list["remote_control_topic"]["NAME"]
+        self.system_shutdown_topic  = prefix + self.topic_list["system_shutdown_topic"]["NAME"]
+        self.vital_topic            = prefix + self.topic_list["vital_topic"]["NAME"]
     
     def broadcast_callback(self, msg: Broadcast):
         if msg.NodeCode == self.NODE_CODES["DRIVE_CONTROL"]:
@@ -208,32 +216,35 @@ class Broadcaster:
     def system_shutdown_callback(self, msg: UInt8):
         if msg.data in [self.SYSTEM_SHUTDOWN_CODE, self.node_code]:
             self.lock.acquire()
-            self.operation = False
-            self.shutdown_flag = True
+            self.operation      = False
+            self.shutdown_flag  = True
             self.lock.release()
     
-    def publish_error_msg(self):
+    def publish_error_msg(self, exc_type, fname, line_no):
         msg = Error()
-        msg.NodeCode = self.node_code
-        msg.ErrorCode = self.err_code
-        msg.Description = self.error_code_list["{}".format(self.err_code)]
+        msg.NodeCode        = self.node_code
+        msg.ErrorCode       = self.err_code
+
+        msg.Info.ExcType    = exc_type
+        msg.Info.Fname      = fname
+        msg.Info.LineNo     = line_no
         
         self.error_pub.publish(msg)
         
     def publish_response(self):
-        msg = NodeRespawnResult()
-        msg.header.stamp = rospy.Time.now()
-        msg.NodeCode = self.node_code
-        msg.Result = self.system_ready
+        msg                 = NodeRespawnResult()
+        msg.header.stamp    = rospy.Time.now()
+        msg.NodeCode        = self.node_code
+        msg.Result          = self.system_ready
         
         self.node_respawn_result_pub.publish(msg)
         
     def publish_vital_chk_msg(self):
         try:
             msg = Vital()
-            msg.NodeCode = self.node_code
-            msg.State = self.system_ready
-            msg.ErrorCode = self.err_code
+            msg.NodeCode    = self.node_code
+            msg.State       = self.system_ready
+            msg.ErrorCode   = self.err_code
             
             self.vital_pub.publish(msg)
             
@@ -250,10 +261,10 @@ class Broadcaster:
                 self.rate1.sleep()
         finally:
             if self.shutdown_flag:
-                self.broadcast_request = self.SOUND_FILE_CODES["SHUT_DOWN_MSG"]
-                sound_file_name = self.sound_file_dir + self.SOUND_FILE_LIST["{}".format(self.broadcast_request)]
-                self.on_air_flag = True
-                self.broadcast_thread = threading.Thread(target=self.broadcast_task, args=(sound_file_name))
+                self.broadcast_request  = self.SOUND_FILE_CODES["SHUT_DOWN_MSG"]
+                sound_file_name         = self.sound_file_dir + self.SOUND_FILE_LIST["{}".format(self.broadcast_request)]
+                self.on_air_flag        = True
+                self.broadcast_thread   = threading.Thread(target=self.broadcast_task, args=(sound_file_name))
                 self.broadcast_thread.start()
                 
                 on_air_flag  = True
